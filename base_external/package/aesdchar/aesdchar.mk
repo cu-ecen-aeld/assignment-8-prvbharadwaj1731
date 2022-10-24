4de6efd6b808e317037c295dcf060071583527f0
prvbharadwaj1731 <prbh1731@colorado.edu>
2022-10-24 01:26:13 -0400

    A8 changes

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -11,6 +11,8 @@ define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 	
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin	
+	
 endef
 
 $(eval $(kernel-module))
